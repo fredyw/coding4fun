dfaa6747a703b06e4b934a20a92b12e4965e341e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-05-31 23:04:04 -0500

    Add problem 29

--- src/main/java/coding4fun/Problem29.java
+++ src/main/java/coding4fun/Problem29.java
@@ -15,19 +15,49 @@ package coding4fun;
  */
 public class Problem29 {
     public static class SegmentTree {
+        private final int[] tree;
         private final int arrayLength;
 
         public SegmentTree(int[] array) {
             arrayLength = array.length;
+            int x = (int) (Math.ceil(Math.log(array.length) / Math.log(2)));
+            int maxSize = 2 * (int) (Math.pow(2, x)) - 1;
+            tree = new int[maxSize];
+            build(array, tree, 0, array.length - 1, 0);
         }
 
-        private static void build() {
-            // TODO:
+        private static int build(int[] array, int[] tree, int start, int end, int current) {
+            if (start == end) {
+                tree[current] = array[start];
+                return tree[current];
+            }
+            int mid = (start + end) / 2;
+            int left = (current * 2) + 1;
+            int right = (current * 2) + 2;
+            int minLeft = build(array, tree, start, mid, left);
+            int minRight = build(array, tree, mid + 1, end, right);
+            tree[current] = Math.min(minLeft, minRight);
+            return tree[current];
         }
 
         public int minRange(int start, int end) {
-            // TODO:
-            return 0;
+            return minRange(start, end, tree, 0, arrayLength - 1, 0);
+        }
+
+        private int minRange(int queryStart, int queryEnd, int[] tree, int start,
+                             int end, int current) {
+            if (queryStart <= start && queryEnd >= end) {
+                return tree[current];
+            }
+            if (queryEnd < start ||  end < queryStart) {
+                return Integer.MAX_VALUE;
+            }
+            int mid = (start + end) / 2;
+            int left = (current * 2) + 1;
+            int right = (current * 2) + 2;
+            int minLeft = minRange(queryStart, queryEnd, tree, start, mid, left);
+            int minRight = minRange(queryStart, queryEnd, tree, mid + 1, end, right);
+            return Math.min(minLeft, minRight);
         }
     }
 
