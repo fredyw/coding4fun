950325f3f0c1b03b7186890dbe9cdc3ddd27bc2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-21 22:01:14 -0500

    Add problem 32

---
+++ src/main/java/coding4fun/Problem32.java
@@ -0,0 +1,43 @@
+package coding4fun;
+
+/**
+ * List all permutations of a given string.
+ *
+ * <pre>
+ * Input : ABC
+ * Output:
+ * ABC
+ * ACB
+ * BAC
+ * BCA
+ * CBA
+ * CAB
+ * </pre>
+ */
+public class Problem32 {
+    private static void permutation(String str) {
+        char[] chars = str.toCharArray();
+        permutation(chars, 0, chars.length);
+    }
+
+    private static void permutation(char[] chars, int left, int right) {
+        if (left == right) {
+            System.out.println(new String(chars));
+            return;
+        }
+        for (int i = left; i < right; i++) {
+            char tmp = chars[left];;
+            chars[left] = chars[i];
+            chars[i] = tmp;
+            permutation(chars, left + 1, right);
+            // backtrack to revert the original list
+            tmp = chars[left];;
+            chars[left] = chars[i];
+            chars[i] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        permutation("ABC");
+    }
+}
